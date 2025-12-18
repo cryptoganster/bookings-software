47035bdb77fc549122fb65f8b41eefc43bb958cc
CryptoGanster - Owner Dev <ganstercrypto@gmail.com>
2025-12-18 16:49:30 -0400

    feat(customer): add domain exceptions

---
+++ apps/backend/src/customer/domain/exceptions/customer-already-linked-to-user.ts
@@ -0,0 +1,17 @@
+/**
+ * Exception thrown when attempting to link a customer that is already linked to a user
+ * 
+ * This exception is thrown when trying to link a customer to a user
+ * but the customer already has a userId (is already registered).
+ */
+export class CustomerAlreadyLinkedToUserException extends Error {
+  constructor(customerId: string) {
+    super(`Customer ${customerId} is already linked to a user`);
+    this.name = 'CustomerAlreadyLinkedToUserException';
+    
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, CustomerAlreadyLinkedToUserException);
+    }
+  }
+}
