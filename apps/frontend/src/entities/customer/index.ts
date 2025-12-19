8ed47a82000b5e43f01d5d7415f78cbfe76bd344
CryptoGanster - Owner Dev <ganstercrypto@gmail.com>
2025-12-19 00:00:30 -0400

    feat(frontend): add customer entity structure

---
+++ apps/frontend/src/entities/customer/index.ts
@@ -0,0 +1,8 @@
+// Customer Entity - Public API
+export * from './model/types';
+export * from './model/useCustomer';
+export * from './model/useCustomers';
+export * from './model/useCustomersByUserId';
+export * from './ui/CustomerCard';
+export * from './ui/CustomerAvatar';
+export * from './ui/CustomerBadge';
